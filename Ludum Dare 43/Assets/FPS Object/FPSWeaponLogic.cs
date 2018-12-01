54b2039e62b0a1905cb4bf0e001a011451923c23
Tomi <tomii.goljevscek@gmail.com>
2018-12-01 15:00:24 +0100

    gameplay

---
+++ Ludum Dare 43/Assets/FPS Object/FPSWeaponLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSWeaponLogic : MonoBehaviour {
+
+
+
+    class Weapon
+    {
+
+    }
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+
+
+
+	}
+}
